d588f894fe118eb9c8f0492059abadfd9bf167fb
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2024-09-21 14:53:55 +0530

    change

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  useEffect(()=>{
+    const phoneNumber = '916377639169'; // Replace with the recipient's phone number
+    const message = 'i want to start RRB Preparation.'; // Replace with your custom message
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    
+    window.open(url, '_blank');
+  },[])
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
